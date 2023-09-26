8c92119c4f162abae0957180671c1ef705a98b02
RubenKorse <121227805+RubenKorse@users.noreply.github.com>
2023-09-26 12:19:28 +0200

    opdracht 12

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tournament;
 use Illuminate\Http\Request;
-
+use Carbon\Carbon;
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $tournaments = Tournament::where('date','>', Carbon::now())->orderBy('date', 'ASC')->first();
+        return view('home')
+                ->with('tournaments', $tournaments);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,15 +15,9 @@
     <p>Zwerkbal (Engels: Quidditch) is een sport voor tovenaars uit de Harry Potterboeken van J.K. Rowling. In de wereld van Harry Potter is Zwerkbal een populaire sport: veel mensen praten erover mee en zijn zeer enthousiast als er een groot toernooi wordt gespeeld. Ook heeft iedere heks of tovenaar wel een favoriet Zwerkbalteam.</p>
 
     <div class="row">
-        <div class="colum">
-            <div class="yellowboder">
-                <p>Aantal aankomende toernooien:</p>
-                <h1>5</h1>
-            </div>
-            <div class="yellowboder">
-                <p>Aantal aankomende toernooien:</p>
-                <h1>5</h1>
-            </div>
+        <div class="yellowboder">
+            <h1>{{ $tournaments->name }}</h1>
+            <p>{{ $tournaments->date }} vanaf {{ $tournaments->start_time }}</p>
         </div>
         <img src="img/snitch.png" alt="photo" class="img">
     </div>
